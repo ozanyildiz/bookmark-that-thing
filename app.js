2e1b1120b2e6a271693f8cfbc0f9cd27b588f545
Ozan Yildiz <ozanyyildiz@gmail.com>
2014-01-20 22:42:16 +0200

    added checkings for tags

--- app.js
+++ app.js
@@ -50,9 +50,18 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
+	var tags = req.body.inputTags.split(",");
+	
+	// Clean tag is the tag with no leading and ending spaces and also no more than one spaces!
+	var cleanTags = [];
+	var cleanTag = null;
+	for (var i = 0; i < tags.length; i++) {
+		cleanTag = tags[i].trim().replace(/ +/g, " ");
+		if (cleanTag != "") { cleanTags.push(cleanTag); }
+	}
 	Bookmark.create({
 		url: req.body.inputUrl,
-		tags: req.body.inputTags.replace(/ /g,'').split(","),
+		tags: cleanTags,
 		notes: req.body.inputNotes
 	}, function(err, bookmark) {
 		if (err) { res.send(err); }
