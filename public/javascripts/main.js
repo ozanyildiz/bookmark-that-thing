28cb853c24a830c0e1bd0a727bb82a96dcde8c4e
Ozan Yildiz <ozanyyildiz@gmail.com>
2014-02-09 16:40:02 +0200

    added accordion form

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
+  $('#newBookmarkForm').hide();
+
   $('[data-toggle=offcanvas]').click(function() {
     $('.row-offcanvas').toggleClass('active');
+  });
+
+  $('#new').click(function() {
+    $('#newBookmarkForm').slideToggle();
   });
 });
