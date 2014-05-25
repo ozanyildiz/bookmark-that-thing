0ee116d63b6e5143d69222813efacba2802d1906
Ozan Yildiz <ozanyyildiz@gmail.com>
2014-05-25 13:25:23 +0300

    changed config for heroku

--- config/config.js
+++ config/config.js
@@ -3,6 +3,6 @@ module.exports = {
     twitter: {
         clientID: process.env.TWITTER_CLIENT_ID,
         clientSecret: process.env.TWITTER_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/twitter/callback"
+        callbackURL: "http://lit-beyond-8956.herokuapp.com/auth/twitter/callback"
     }
 }
